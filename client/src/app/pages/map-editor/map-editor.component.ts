de6a6d702dd141c110855a530e71b3c0bccc844b
iremyildizz <iremyildiz02@gmail.com>
2024-09-09 20:39:17 -0400

    Feat(navigation): Navigation a page map-editor

---
+++ client/src/app/pages/map-editor/map-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-editor.component.html',
+  styleUrl: './map-editor.component.scss'
+})
+export class MapEditorComponent {
+
+}
