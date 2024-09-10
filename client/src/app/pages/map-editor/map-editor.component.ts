3e14bdc6ed73dba87c04a960f66241cad00e9a60
iremyildizz <iremyildiz02@gmail.com>
2024-09-10 18:22:37 -0400

    feat(visual): tiles and tile colors added

--- client/src/app/pages/map-editor/map-editor.component.ts
+++ client/src/app/pages/map-editor/map-editor.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-map-editor',
-  standalone: true,
-  imports: [],
-  templateUrl: './map-editor.component.html',
-  styleUrl: './map-editor.component.scss'
+    selector: 'app-map-editor',
+    standalone: true,
+    imports: [],
+    templateUrl: './map-editor.component.html',
+    styleUrl: './map-editor.component.scss',
 })
 export class MapEditorComponent {
-
+    grid = [
+        [{ type: 'stone' }, { type: 'water' }, { type: 'grass' }, { type: 'stone' }, { type: 'water' }],
+        [{ type: 'grass' }, { type: 'stone' }, { type: 'water' }, { type: 'grass' }, { type: 'stone' }],
+        [{ type: 'water' }, { type: 'grass' }, { type: 'stone' }, { type: 'water' }, { type: 'grass' }],
+        [{ type: 'stone' }, { type: 'water' }, { type: 'grass' }, { type: 'stone' }, { type: 'water' }],
+        [{ type: 'grass' }, { type: 'stone' }, { type: 'water' }, { type: 'grass' }, { type: 'stone' }],
+    ];
 }
