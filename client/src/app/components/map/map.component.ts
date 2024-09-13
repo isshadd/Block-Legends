57b01569976247fa756e7f89927fb3dbdc16a7ec
Pablo <pablo.cabale@polymtl.ca>
2024-09-13 13:41:14 -0400

    feat(component): map is now a component

---
+++ client/src/app/components/map/map.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { BaseTile } from '@app/classes/Tiles/base-tile';
+import { DoorTile } from '@app/classes/Tiles/door-tile';
+import { IceTile } from '@app/classes/Tiles/ice-tile';
+import { WallTile } from '@app/classes/Tiles/wall-tile';
+import { WaterTile } from '@app/classes/Tiles/water-tile';
+
+@Component({
+    selector: 'app-map',
+    standalone: true,
+    imports: [],
+    templateUrl: './map.component.html',
+    styleUrl: './map.component.scss',
+})
+export class MapComponent {
+    gridType: Array<BaseTile> = [new WaterTile(), new DoorTile(), new IceTile(), new WallTile(), new BaseTile()];
+    grid: Array<Array<BaseTile>> = [];
+
+    gridCreator(tileNumber: number) {
+        for (let i = 0; i < tileNumber; i++) {
+            this.grid.push([]);
+            for (let j = 0; j < tileNumber; j++) {
+                this.grid[i].push(this.gridType[Math.floor(Math.random() * this.gridType.length)]);
+            }
+        }
+    }
+
+    constructor() {
+        this.gridCreator(20);
+    }
+
+    onTileClick(i: number, j: number, tile: BaseTile) {
+        alert(`${tile.name} ${j}, ${i} clicked`);
+    }
+}
--- client/src/app/pages/map-editor/map-editor.component.ts
+++ client/src/app/pages/map-editor/map-editor.component.ts
@@ -6,34 +6,16 @@ import { DoorTile } from '@app/classes/Tiles/door-tile';
 import { IceTile } from '@app/classes/Tiles/ice-tile';
 import { WallTile } from '@app/classes/Tiles/wall-tile';
 import { WaterTile } from '@app/classes/Tiles/water-tile';
+import { MapComponent } from '@app/components/map/map.component';
 
 @Component({
     selector: 'app-map-editor',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, MapComponent],
     templateUrl: './map-editor.component.html',
     styleUrl: './map-editor.component.scss',
 })
 export class MapEditorComponent {
-    gridType: Array<BaseTile> = [new WaterTile(), new DoorTile(), new IceTile(), new WallTile(), new BaseTile()];
-    grid: Array<Array<BaseTile>> = [];
-
-    gridCreator(tileNumber: number) {
-        for (let i = 0; i < tileNumber; i++) {
-            this.grid.push([]);
-            for (let j = 0; j < tileNumber; j++) {
-                this.grid[i].push(this.gridType[Math.floor(Math.random() * this.gridType.length)]);
-            }
-        }
-    }
-
     itemTypes: Array<Item> = [new Item(), new Item(), new Item(), new Item(), new Item(), new Item()];
-
-    constructor() {
-        this.gridCreator(20);
-    }
-
-    onTileClick(i: number, j: number, tile: BaseTile) {
-        alert(`${tile.name} ${j}, ${i} clicked`);
-    }
+    gridType: Array<BaseTile> = [new WaterTile(), new DoorTile(), new IceTile(), new WallTile(), new BaseTile()];
 }
