1771fbec3d2d947c0d3ba07e3ddc67387e131aa6
Pablo <pablo.cabale@polymtl.ca>
2024-09-14 11:42:28 -0400

    feat(component): placeable entity container base files

---
+++ client/src/app/components/map-editor-components/placeable-entity-container/placeable-entity-container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlaceableEntityContainerComponent } from './placeable-entity-container.component';
+
+describe('PlaceableEntityContainerComponent', () => {
+  let component: PlaceableEntityContainerComponent;
+  let fixture: ComponentFixture<PlaceableEntityContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PlaceableEntityContainerComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PlaceableEntityContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/map-editor-components/placeable-entity-container/placeable-entity-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeable-entity-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './placeable-entity-container.component.html',
+  styleUrl: './placeable-entity-container.component.scss'
+})
+export class PlaceableEntityContainerComponent {
+
+}
