5a37186121a6f7fce2c48e635128835a5ac2203f
Pablo <pablo.cabale@polymtl.ca>
2024-09-18 11:10:36 -0400

    feat: Tooltip added for items in list

--- client/src/app/components/map-editor-components/placeable-entity-container/placeable-entity-container.component.ts
+++ client/src/app/components/map-editor-components/placeable-entity-container/placeable-entity-container.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { PlaceableEntityComponent } from '@app/components/game-board-components/placeable-entity/placeable-entity.component';
 import { PlaceableEntity } from '@app/interfaces/placeable-entity';
 
 @Component({
     selector: 'app-placeable-entity-container',
     standalone: true,
-    imports: [PlaceableEntityComponent],
+    imports: [PlaceableEntityComponent, MatTooltipModule],
     templateUrl: './placeable-entity-container.component.html',
     styleUrl: './placeable-entity-container.component.scss',
 })
