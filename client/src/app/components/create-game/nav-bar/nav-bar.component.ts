e1c764b9459ef0e52ae3d767375a5d16148ceb54
Sidali Boukhezar <sidali.boukhezar@gmail.com>
2024-09-25 19:41:55 -0400

    feat(nav-bar): refactor create-game in secondly nav-bar

---
+++ client/src/app/components/create-game/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ModeService } from '@app/services/gameMode.service';
+
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+  selectedMode: string | null = 'Combat classique';
+
+  constructor(private modeService: ModeService) {}
+
+  selectMode(mode: string): void {
+    this.selectedMode = mode;
+    this.modeService.setSelectedMode(mode);
+  }
+}
