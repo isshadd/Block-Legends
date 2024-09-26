495d300e00980b7505d65d61a9277411ad4892c4
isshadd <issam.haddadi@polymtl.ca>
2024-09-25 20:13:58 -0400

    fix(tests): create-game

--- client/src/app/components/create-game/game-list/game-list/game-list.component.spec.ts
+++ client/src/app/components/create-game/game-list/game-list/game-list.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { NavBarComponent } from '@app/components/create-game/nav-bar/nav-bar.component';
+import { ModeService } from '@app/services/gameMode.service';
+import { Game } from '@common/game.interface';
+import { of } from 'rxjs';
 import { GameListComponent } from './game-list.component';
+
+const GAME_SIZE = 30;
 
 describe('GameListComponent', () => {
     let component: GameListComponent;
     let fixture: ComponentFixture<GameListComponent>;
+    let mockRouter: jasmine.SpyObj<Router>;
+    let mockModeService: jasmine.SpyObj<ModeService>;
 
     beforeEach(async () => {
+        mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+        mockModeService = jasmine.createSpyObj('ModeService', ['selectedMode$']);
+        mockModeService.selectedMode$ = of('Combat classique');
         await TestBed.configureTestingModule({
             imports: [GameListComponent],
+            providers: [{ provide: Router, useValue: mockRouter }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(GameListComponent);
@@ -18,5 +30,77 @@ describe('GameListComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should select a game', () => {
+        const game: Game = {
+            id: 0,
+            name: 'JeuTest',
+            size: GAME_SIZE,
+            mode: 'Combat classique',
+            imageUrl: '',
+            lastModificationDate: new Date('2024-10-23'),
+            isVisible: true,
+        };
+        component.selectGame(game);
+        expect(component.selectedGame).toEqual(game);
+    });
+
+    it('should navigate to the home page', () => {
+        component.homeButton();
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/home']);
+    });
+
+    it('should navigate to the create-character page', () => {
+        const game: Game = {
+            id: 0,
+            name: 'JeuTest',
+            size: GAME_SIZE,
+            mode: 'Combat classique',
+            imageUrl: '',
+            lastModificationDate: new Date('2024-10-23'),
+            isVisible: true,
+        };
+        component.selectGame(game);
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/create-character']);
+    });
+
+    it('should hide the game if it is not visible', () => {
+        const game: Game = {
+            id: 0,
+            name: 'JeuTest',
+            size: GAME_SIZE,
+            mode: 'Combat classique',
+            imageUrl: '',
+            lastModificationDate: new Date('2024-10-23'),
+            isVisible: false,
+        };
+        component.selectGame(game);
+        expect(component.selectedGame).toBeNull();
+        expect(component.gameStatus).toEqual(`Le jeu choisi ${game.name} n'est plus visible ou supprimé`);
+    });
+
+    it('should select a mode', () => {
+        const mode = 'Combat classique';
+        const navbar = new NavBarComponent(new ModeService());
+        navbar.selectMode(mode);
+        expect(component.selectedMode).toEqual(mode);
+    });
+
+    it('should filter games by mode', () => {
+        const games: Game[] = [
+            {
+                id: 0,
+                name: 'JeuTest',
+                size: GAME_SIZE,
+                mode: 'Combat classique',
+                imageUrl: '',
+                lastModificationDate: new Date('2024-10-23'),
+                isVisible: true,
+            },
+        ];
+        component.games = games;
+        component.selectedMode = null;
+        expect(component.getFilteredGames()).toEqual([games[0]]);
     });
 });
--- client/src/app/pages/create-game/create-game.component.spec.ts
+++ client/src/app/pages/create-game/create-game.component.spec.ts
@@ -1,20 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
-import { Game } from '@common/game.interface';
 import { CreateGameComponent } from './create-game.component';
-
-const GAME_SIZE = 30;
 
 describe('CreateGameComponent', () => {
     let component: CreateGameComponent;
     let fixture: ComponentFixture<CreateGameComponent>;
-    let mockRouter: jasmine.SpyObj<Router>;
 
     beforeEach(async () => {
-        mockRouter = jasmine.createSpyObj('Router', ['navigate']);
         await TestBed.configureTestingModule({
             imports: [CreateGameComponent],
-            providers: [{ provide: Router, useValue: mockRouter }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(CreateGameComponent);
@@ -24,76 +17,5 @@ describe('CreateGameComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('should select a game', () => {
-        const game: Game = {
-            id: 0,
-            name: 'JeuTest',
-            size: 30,
-            mode: 'Combat classique',
-            imageUrl: '',
-            lastModificationDate: new Date('2024-10-23'),
-            isVisible: true,
-        };
-        component.selectGame(game);
-        expect(component.selectedGame).toEqual(game);
-    });
-
-    it('should navigate to the home page', () => {
-        component.homeButton();
-        expect(mockRouter.navigate).toHaveBeenCalledWith(['/home']);
-    });
-
-    it('should navigate to the create-character page', () => {
-        const game: Game = {
-            id: 0,
-            name: 'JeuTest',
-            size: 30,
-            mode: 'Combat classique',
-            imageUrl: '',
-            lastModificationDate: new Date('2024-10-23'),
-            isVisible: true,
-        };
-        component.selectGame(game);
-        expect(mockRouter.navigate).toHaveBeenCalledWith(['/create-character']);
-    });
-
-    it('should hide the game if it is not visible', () => {
-        const game: Game = {
-            id: 0,
-            name: 'JeuTest',
-            size: 30,
-            mode: 'Combat classique',
-            imageUrl: '',
-            lastModificationDate: new Date('2024-10-23'),
-            isVisible: false,
-        };
-        component.selectGame(game);
-        expect(component.selectedGame).toBeNull();
-        expect(component.gameStatus).toEqual(`Le jeu choisi ${game.name} n'est plus visible ou supprimé`);
-    });
-
-    it('should select a mode', () => {
-        const mode = 'Combat classique';
-        component.selectMode(mode);
-        expect(component.selectedMode).toEqual(mode);
-    });
-
-    it('should filter games by mode', () => {
-        const games: Game[] = [
-            {
-                id: 0,
-                name: 'JeuTest',
-                size: GAME_SIZE,
-                mode: 'Combat classique',
-                imageUrl: '',
-                lastModificationDate: new Date('2024-10-23'),
-                isVisible: true,
-            },
-        ];
-        component.games = games;
-        component.selectedMode = null;
-        expect(component.getFilteredGames()).toEqual([games[0]]);
     });
 });
