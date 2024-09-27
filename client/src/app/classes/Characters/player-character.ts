e05d67e670947b3bc3ea18b31df7327527faee2a
Sidali Boukhezar <sidali.boukhezar@gmail.com>
2024-09-27 16:44:54 -0400

    fix(create-character): assignDice() logic fixed

--- client/src/app/classes/Characters/player-character.ts
+++ client/src/app/classes/Characters/player-character.ts
@@ -6,8 +6,8 @@ export const BASE_STATS = 4;
 export class PlayerCharacter {
     isLifeBonusAssigned: boolean = false;
     isSpeedBonusAssigned: boolean = false;
-    isAttackBonusAssigned: boolean = false;
-    isDefenseBonusAssigned: boolean = false;
+    isAttackBonusAssigned: boolean = true;
+    isDefenseBonusAssigned: boolean = true;
     isOrganizer: boolean = false;
     dice: string;
     constructor(
