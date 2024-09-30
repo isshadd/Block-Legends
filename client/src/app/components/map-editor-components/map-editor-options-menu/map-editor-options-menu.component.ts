23b483b926a28cfcbc4217e9b2fa4a775124651a
Pablo <pablo.cabale@polymtl.ca>
2024-09-29 22:20:18 -0400

    Feature: tooltip for map editor buttons

--- client/src/app/components/map-editor-components/map-editor-options-menu/map-editor-options-menu.component.ts
+++ client/src/app/components/map-editor-components/map-editor-options-menu/map-editor-options-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 import { GameMapDataManagerService } from '@app/services/game-board-services/game-map-data-manager.service';
 import { GameShared } from '@common/interfaces/game-shared';
 import { ButtonNotificationComponent, ButtonNotificationState } from '../button-notification/button-notification.component';
@@ -9,13 +10,16 @@ import { MapEditorModalComponent } from '../map-editor-modal/map-editor-modal.co
 @Component({
     selector: 'app-map-editor-options-menu',
     standalone: true,
-    imports: [MatIconModule, ButtonNotificationComponent],
+    imports: [MatIconModule, ButtonNotificationComponent, MatTooltip],
     templateUrl: './map-editor-options-menu.component.html',
     styleUrl: './map-editor-options-menu.component.scss',
 })
 export class MapEditorOptionsMenuComponent {
     optionsNotificationState = ButtonNotificationState.HIDDEN;
     saveNotificationState = ButtonNotificationState.HIDDEN;
+
+    optionsNotificationDescription = '';
+    saveNotificationDescription = '';
 
     constructor(
         public gameMapDataManagerService: GameMapDataManagerService,
@@ -50,19 +54,24 @@ export class MapEditorOptionsMenuComponent {
 
     getOptionsNotificationState(): ButtonNotificationState {
         if (!this.gameMapDataManagerService.hasValidNameAndDescription()) {
+            this.optionsNotificationDescription = 'Il faut donner un nom et une description à la carte';
             return ButtonNotificationState.ALERT;
         } else {
+            this.optionsNotificationDescription = '';
             return ButtonNotificationState.HIDDEN;
         }
     }
 
     getSaveNotificationState(): ButtonNotificationState {
         if (!this.gameMapDataManagerService.isSavedGame()) {
+            this.saveNotificationDescription = "La carte n'est pas sauvegardée";
             return ButtonNotificationState.ALERT;
         }
         if (this.gameMapDataManagerService.isGameUpdated) {
+            this.saveNotificationDescription = 'La carte a été modifiée';
             return ButtonNotificationState.WARNING;
         }
+        this.saveNotificationDescription = 'La carte est sauvegardée';
         return ButtonNotificationState.SUCCESS;
     }
 }
