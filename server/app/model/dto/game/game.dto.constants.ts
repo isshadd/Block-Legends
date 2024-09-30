0a2757f28b2a5c5a64d13833aaca200a683978a6
Pablo <pablo.cabale@polymtl.ca>
2024-09-30 16:41:31 -0400

    Fix: server-side lint passed

--- server/app/model/dto/game/game.dto.constants.ts
+++ server/app/model/dto/game/game.dto.constants.ts
@@ -2,8 +2,8 @@ import { GameMode } from '@common/enums/game-mode';
 import { MapSize } from '@common/enums/map-size';
 
 export const MAP_CONSTANTS = {
-    NAME_MAX_LENGTH: 50,
-    DESCRIPTION_MAX_LENGTH: 255,
-    ALLOWED_SIZES: [MapSize.SMALL, MapSize.MEDIUM, MapSize.LARGE] as const,
-    ALLOWED_MODES: [GameMode.CTF, GameMode.Classique] as const,
+    nameMaxLength: 50,
+    descriptionMaxLength: 255,
+    allowedSizes: [MapSize.SMALL, MapSize.MEDIUM, MapSize.LARGE] as const,
+    allowedModes: [GameMode.CTF, GameMode.Classique] as const,
 };
