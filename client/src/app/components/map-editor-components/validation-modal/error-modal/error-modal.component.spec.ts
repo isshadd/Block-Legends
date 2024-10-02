938788f45a28cac6033aef718ea1596aedc1e9e5
Pablo <pablo.cabale@polymtl.ca>
2024-10-01 22:44:20 -0400

    Fix: error component

--- client/src/app/components/map-editor-components/validation-modal/error-modal/error-modal.component.spec.ts
+++ client/src/app/components/map-editor-components/validation-modal/error-modal/error-modal.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ErrorModalComponent } from './error-modal.component';
 
 describe('ErrorModalComponent', () => {
@@ -8,20 +8,19 @@ describe('ErrorModalComponent', () => {
     let dialogRefSpy: jasmine.SpyObj<MatDialogRef<ErrorModalComponent>>;
 
     beforeEach(async () => {
-        // Create a spy for MatDialogRef to mock its behavior
         dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
 
         await TestBed.configureTestingModule({
             imports: [ErrorModalComponent],
             providers: [
-                { provide: MatDialogRef, useValue: dialogRefSpy }, // Mock MatDialogRef
-                { provide: MAT_DIALOG_DATA, useValue: { message: 'Test error message' } }, // Provide mock data for the dialog
+                { provide: MatDialogRef, useValue: dialogRefSpy },
+                { provide: MAT_DIALOG_DATA, useValue: { message: 'Test error message' } },
             ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ErrorModalComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges(); // Trigger initial data binding
+        fixture.detectChanges();
     });
 
     it('should create', () => {
@@ -29,7 +28,8 @@ describe('ErrorModalComponent', () => {
     });
 
     it('should call dialogRef.close() when close() is called', () => {
-        component.close(); // Call the close method
-        expect(dialogRefSpy.close).toHaveBeenCalled(); // Ensure close() is called on the MatDialogRef spy
+        component.close();
+        dialogRefSpy.close.and.callThrough();
+        expect(dialogRefSpy.close).toHaveBeenCalled();
     });
 });
