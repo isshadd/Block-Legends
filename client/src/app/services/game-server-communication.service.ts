bab63138298239410e0f7a17f2cf102715a1755f
Nicolas Bilodeau <nicco.bilodeau@gmail.com>
2024-10-01 20:46:07 -0400

    Fix handleErrors

--- client/src/app/services/game-server-communication.service.ts
+++ client/src/app/services/game-server-communication.service.ts
@@ -24,11 +24,11 @@ export class GameServerCommunicationService {
     }
 
     addGame(game: CreateGameSharedDto): Observable<GameShared> {
-        return this.http.post<GameShared>(`${this.baseUrl}/`, game).pipe(catchError(this.handleError<GameShared>()));
+        return this.http.post<GameShared>(`${this.baseUrl}/`, game).pipe(catchError(this.handleErrors<GameShared>()));
     }
 
     updateGame(id: string, game: UpdateGameSharedDto): Observable<void> {
-        return this.http.patch<void>(`${this.baseUrl}/${id}`, game).pipe(catchError(this.handleError<void>()));
+        return this.http.patch<void>(`${this.baseUrl}/${id}`, game).pipe(catchError(this.handleErrors<void>()));
     }
 
     deleteGame(id: string): Observable<void> {
