710fa72a25c48315967b3d9fa9e9cb4cdc0d5d3a
Sidali Boukhezar <sidali.boukhezar@gmail.com>
2024-10-11 14:00:16 -0400

    fix(create-game): fix game deleted or notvisible in game's list

--- client/src/app/components/create-game/game-list/game-list/game-list.component.ts
+++ client/src/app/components/create-game/game-list/game-list/game-list.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common'; // Importez CommonModule
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalOneOptionComponent } from '@app/components/modal-one-option/modal-one-option.component';
 import { AdministrationPageManagerService } from '@app/services/administration-page-services/administration-page-manager.services';
 import { ModeService } from '@app/services/game-mode-services/gameMode.service';
 import { GameServerCommunicationService } from '@app/services/game-server-communication.service';
@@ -10,7 +11,7 @@ import { GameShared } from '@common/interfaces/game-shared';
 @Component({
     selector: 'app-game-list',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ModalOneOptionComponent],
     templateUrl: './game-list.component.html',
     styleUrl: './game-list.component.scss',
 })
@@ -19,6 +20,7 @@ export class GameListComponent implements OnInit {
     selectedGame: GameShared | null;
     gameStatus: string | null;
     selectedMode: GameMode = GameMode.Classique;
+    isModalOpen = false;
 
     constructor(
         private modeService: ModeService,
@@ -46,8 +48,8 @@ export class GameListComponent implements OnInit {
 
     selectGame(game: GameShared) {
         this.gameServerCommunicationService.getGame(game._id).subscribe((updatedGame: GameShared) => {
-            if (!updatedGame.isVisible) {
-                this.gameStatus = `Le jeu choisi ${updatedGame.name} n'est plus disponible`;
+            if (!updatedGame || !updatedGame.isVisible ) {
+                this.gameStatus = `Le jeu choisi ${updatedGame ? updatedGame.name : ''} n'est plus disponible`;
                 this.selectedGame = null;
             } else {
                 this.selectedGame = updatedGame;
@@ -58,6 +60,22 @@ export class GameListComponent implements OnInit {
     }
 
     getFilteredGames() {
-        return this.games.filter((game) => game.isVisible && game.mode === this.selectedMode);
+        return this.games.filter((game) => game.isVisible && game.mode === this.selectedMode && game != null);
     }
+
+    openModal() {
+        this.isModalOpen = true;
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+        this.gameStatus = null; // Réinitialiser le message d'erreur
+    }
+
+    confirmBack() {
+        this.closeModal();
+        // this.router.navigate(['/create-game']);
+        window.location.reload();
+    }
+
 }
