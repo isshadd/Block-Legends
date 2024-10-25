d84e9cac8bdd44b6d95a116a2634603a8d9707c6
iremyildizz <iremyildiz02@gmail.com>
2024-10-25 19:38:44 -0400

    feat:playerMapEntity

---
+++ client/src/app/classes/Characters/player-map-entity.spec.ts
@@ -0,0 +1,7 @@
+import { PlayerMapEntity } from './player-map-entity';
+
+describe('PlayerMapEntity', () => {
+  it('should create an instance', () => {
+    expect(new PlayerMapEntity()).toBeTruthy();
+  });
+});
