bd2c5fc36eeec780984c60fc659cae7c90386a79
Pablo <pablo.cabale@polymtl.ca>
2024-10-24 20:05:09 -0400

    Test: test for map editor pages done

--- client/src/app/pages/map-editor/map-editor.component.spec.ts
+++ client/src/app/pages/map-editor/map-editor.component.spec.ts
@@ -1,6 +1,9 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DiamondSword } from '@app/classes/Items/diamond-sword';
+import { GrassTile } from '@app/classes/Tiles/grass-tile';
+import { WallTile } from '@app/classes/Tiles/wall-tile';
 import { GameMapDataManagerService } from '@app/services/game-board-services/game-map-data-manager.service';
 import { GameServerCommunicationService } from '@app/services/game-server-communication.service';
 import { MapEditorManagerService } from '@app/services/map-editor-services/map-editor-manager.service';
@@ -40,6 +43,8 @@ describe('MapEditorComponent', () => {
         isVisible: false,
         tiles: [[{ type: TileType.Grass }], [{ type: TileType.Grass }]],
     };
+
+    const testingMouseCoordinates = { clientX: 100, clientY: 200 };
 
     beforeEach(async () => {
         const routerSpyObj = jasmine.createSpyObj('Router', ['navigate']);
@@ -125,5 +130,187 @@ describe('MapEditorComponent', () => {
 
             expect(routerSpy.navigate).toHaveBeenCalledWith(['/administration-game']);
         });
+    });
+
+    it('should call mapEditorManagerService.onMouseUp and set isDragging to false', () => {
+        component.isDragging = true;
+
+        component.onMouseUp();
+
+        expect(mapEditorManagerServiceSpy.onMouseUp).toHaveBeenCalled();
+        expect(component.isDragging).toBeFalse();
+    });
+
+    it('should call mapEditorManagerService.onMapMouseEnter', () => {
+        component.onMapMouseEnter();
+
+        expect(mapEditorManagerServiceSpy.onMapMouseEnter).toHaveBeenCalled();
+    });
+
+    it('should call mapEditorManagerService.onMapMouseLeave', () => {
+        component.onMapMouseLeave();
+
+        expect(mapEditorManagerServiceSpy.onMapMouseLeave).toHaveBeenCalled();
+    });
+
+    it('should call mapEditorManagerService.onMouseUpMapTile with the correct tile', () => {
+        const mockTile = new GrassTile();
+
+        component.onMapTileMouseUp(mockTile);
+
+        expect(mapEditorManagerServiceSpy.onMouseUpMapTile).toHaveBeenCalledWith(mockTile);
+    });
+
+    describe('onMouseDown', () => {
+        it('should not set dragging state if right mouse button is clicked', () => {
+            const mockEvent = new MouseEvent('mousedown', { button: 2 });
+
+            component.onMouseDown(mockEvent);
+
+            expect(mapEditorManagerServiceSpy.getDraggedItem).not.toHaveBeenCalled();
+        });
+
+        it('should set dragging state and dragImage when left button is pressed and draggedItem exists', () => {
+            const mockEvent = new MouseEvent('mousedown', {
+                button: 0,
+                clientX: testingMouseCoordinates.clientX,
+                clientY: testingMouseCoordinates.clientY,
+            });
+            const draggedItem = new DiamondSword();
+
+            mapEditorManagerServiceSpy.getDraggedItem.and.returnValue(draggedItem);
+            spyOn(component, 'onMouseMove');
+            spyOn(mockEvent, 'preventDefault');
+
+            component.onMouseDown(mockEvent);
+
+            expect(mockEvent.preventDefault).toHaveBeenCalled();
+            expect(mapEditorManagerServiceSpy.getDraggedItem).toHaveBeenCalled();
+            expect(component.isDragging).toBeTrue();
+            expect(component.dragImage).toBe(draggedItem.imageUrl);
+            expect(component.onMouseMove).toHaveBeenCalledWith(mockEvent);
+        });
+
+        it('should not set dragging state if no draggedItem exists', () => {
+            const mockEvent = new MouseEvent('mousedown', {
+                button: 0,
+                clientX: testingMouseCoordinates.clientX,
+                clientY: testingMouseCoordinates.clientY,
+            });
+            mapEditorManagerServiceSpy.getDraggedItem.and.returnValue(null);
+            spyOn(component, 'onMouseMove');
+            spyOn(mockEvent, 'preventDefault');
+
+            component.onMouseDown(mockEvent);
+
+            expect(mockEvent.preventDefault).toHaveBeenCalled();
+            expect(mapEditorManagerServiceSpy.getDraggedItem).toHaveBeenCalled();
+            expect(component.onMouseMove).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('onMouseMove', () => {
+        it('should update mouseX and mouseY when dragging is true', () => {
+            component.isDragging = true;
+            const mockEvent = new MouseEvent('mousemove', testingMouseCoordinates);
+
+            component.onMouseMove(mockEvent);
+
+            expect(component.mouseX).toBe(testingMouseCoordinates.clientX);
+            expect(component.mouseY).toBe(testingMouseCoordinates.clientY);
+        });
+
+        it('should not update mouseX and mouseY when dragging is false', () => {
+            component.isDragging = false;
+            const mockEvent = new MouseEvent('mousemove', testingMouseCoordinates);
+
+            component.onMouseMove(mockEvent);
+
+            expect(component.mouseX).toBe(0);
+            expect(component.mouseY).toBe(0);
+        });
+    });
+
+    describe('onMapTileMouseDown', () => {
+        it('should initiate dragging if tile is TerrainTile with an item', () => {
+            const mockEvent = new MouseEvent('mousedown', {
+                button: 0,
+                clientX: testingMouseCoordinates.clientX,
+                clientY: testingMouseCoordinates.clientY,
+            });
+            const mockItem = new DiamondSword();
+            const mockTerrainTile = new GrassTile();
+            mockTerrainTile.item = mockItem;
+
+            mapEditorManagerServiceSpy.onMouseDownMapTile.and.callThrough();
+            spyOn(component, 'onMouseMove');
+            spyOn(mockEvent, 'preventDefault');
+
+            component.onMapTileMouseDown(mockEvent, mockTerrainTile);
+
+            expect(mapEditorManagerServiceSpy.onMouseDownMapTile).toHaveBeenCalledWith(mockEvent, mockTerrainTile);
+            expect(component.isDragging).toBeTrue();
+            expect(component.dragImage).toBe(mockItem.imageUrl);
+            expect(component.onMouseMove).toHaveBeenCalledWith(mockEvent);
+            expect(mockEvent.preventDefault).toHaveBeenCalled();
+        });
+
+        it('should not initiate dragging if tile is not TerrainTile', () => {
+            const mockEvent = new MouseEvent('mousedown', {
+                button: 0,
+                clientX: testingMouseCoordinates.clientX,
+                clientY: testingMouseCoordinates.clientY,
+            });
+            const mockTile = new WallTile();
+            spyOn(component, 'onMouseMove');
+
+            component.onMapTileMouseDown(mockEvent, mockTile);
+
+            expect(mapEditorManagerServiceSpy.onMouseDownMapTile).toHaveBeenCalledWith(mockEvent, mockTile);
+            expect(component.isDragging).toBeFalse();
+            expect(component.dragImage).toBe('');
+            expect(component.onMouseMove).not.toHaveBeenCalled();
+        });
+
+        it('should not initiate dragging if TerrainTile has no item', () => {
+            const mockEvent = new MouseEvent('mousedown', {
+                button: 0,
+                clientX: testingMouseCoordinates.clientX,
+                clientY: testingMouseCoordinates.clientY,
+            });
+            const mockTerrainTile = new GrassTile();
+            spyOn(component, 'onMouseMove');
+
+            component.onMapTileMouseDown(mockEvent, mockTerrainTile);
+
+            expect(mapEditorManagerServiceSpy.onMouseDownMapTile).toHaveBeenCalledWith(mockEvent, mockTerrainTile);
+            expect(component.isDragging).toBeFalse();
+            expect(component.dragImage).toBe('');
+            expect(component.onMouseMove).not.toHaveBeenCalled();
+        });
+    });
+
+    it('should call mapEditorManagerService.onMouseEnter with the correct tile', () => {
+        const mockTile = new GrassTile();
+
+        component.onMapTileMouseEnter(mockTile);
+
+        expect(mapEditorManagerServiceSpy.onMouseEnter).toHaveBeenCalledWith(mockTile);
+    });
+
+    it('should call mapEditorManagerService.onMouseMoveMapTile with the correct tile', () => {
+        const mockTile = new GrassTile();
+
+        component.onMapTileMouseMove(mockTile);
+
+        expect(mapEditorManagerServiceSpy.onMouseMoveMapTile).toHaveBeenCalledWith(mockTile);
+    });
+
+    it('should call mapEditorManagerService.onMouseLeave with the correct tile', () => {
+        const mockTile = new GrassTile();
+
+        component.onMapTileMouseLeave(mockTile);
+
+        expect(mapEditorManagerServiceSpy.onMouseLeave).toHaveBeenCalledWith(mockTile);
     });
 });
