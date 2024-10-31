b2c34c4fce9e4bba06f2078883ab6c4cd1674a29
iremyildizz <iremyildiz02@gmail.com>
2024-10-31 15:14:14 -0400

    test:infoView

--- client/src/app/components/player-map-entity-info-view/player-map-entity-info-view.component.spec.ts
+++ client/src/app/components/player-map-entity-info-view/player-map-entity-info-view.component.spec.ts
@@ -1,23 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { PlayerCharacter } from '@app/classes/Characters/player-character';
 import { PlayerMapEntityInfoViewComponent } from './player-map-entity-info-view.component';
 
 describe('PlayerMapEntityInfoViewComponent', () => {
-  let component: PlayerMapEntityInfoViewComponent;
-  let fixture: ComponentFixture<PlayerMapEntityInfoViewComponent>;
+    let component: PlayerMapEntityInfoViewComponent;
+    let fixture: ComponentFixture<PlayerMapEntityInfoViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PlayerMapEntityInfoViewComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [PlayerMapEntityInfoViewComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(PlayerMapEntityInfoViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(PlayerMapEntityInfoViewComponent);
+        component = fixture.componentInstance;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        component.playerCharacter = new PlayerCharacter('TestCharacter');
+        component.playerCharacter.attributes = {
+            life: 5,
+            defense: 3,
+            speed: 4,
+            attack: 6,
+        };
+        fixture.detectChanges();
+    });
+
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should return an array of the correct length for healthArray', () => {
+        expect(component.healthArray.length).toBe(5);
+    });
+
+    it('should return an array of the correct length for defenseArray', () => {
+        expect(component.defenseArray.length).toBe(3);
+    });
+
+    it('should return an array of the correct length for speedArray', () => {
+        expect(component.speedArray.length).toBe(4);
+    });
+
+    it('should return an array of the correct length for attackArray', () => {
+        expect(component.attackArray.length).toBe(6);
+    });
 });
