8a7e9ab033d4f6020099c99a679fcfdf3983f76e
Nihal-T <taihnihal1@gmail.com>
2024-10-31 19:56:45 -0400

    feat(vue-jeu): ajout infos-player et tableau des victoires et défaites

---
+++ client/src/app/components/players-list/players-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-players-list',
+    standalone: true,
+    imports: [],
+    templateUrl: './players-list.component.html',
+    styleUrl: './players-list.component.scss',
+})
+export class PlayersListComponent {
+    players = [
+        { name: 'Sam', health: 100, speed: 10, attack: 5, victories: 3, defeats: 1 },
+        { name: 'Béatrice', health: 90, speed: 12, attack: 6, victories: 5, defeats: 2 },
+        { name: 'Amine', health: 80, speed: 9, attack: 7, victories: 2, defeats: 4 },
+        { name: 'Sam', health: 100, speed: 10, attack: 5, victories: 3, defeats: 1 },
+        { name: 'Béatrice', health: 90, speed: 12, attack: 6, victories: 5, defeats: 2 },
+        { name: 'Amine', health: 80, speed: 9, attack: 7, victories: 2, defeats: 4 },
+        { name: 'Sam', health: 100, speed: 10, attack: 5, victories: 3, defeats: 1 },
+        { name: 'Béatrice', health: 90, speed: 12, attack: 6, victories: 5, defeats: 2 },
+        { name: 'Amine', health: 80, speed: 9, attack: 7, victories: 2, defeats: 4 },
+    ];
+}
--- client/src/app/pages/play-page/play-page.component.ts
+++ client/src/app/pages/play-page/play-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MapComponent } from '@app/components/game-board-components/map/map.component';
+import { PlayerInfoComponent } from '@app/components/player-info/player-info.component';
+import { PlayersListComponent } from '@app/components/players-list/players-list.component';
 import { PlayGameBoardManagerService } from '@app/services/play-page-services/game-board/play-game-board-manager.service';
 
 @Component({
     selector: 'app-play-page',
     standalone: true,
-    imports: [MapComponent, RouterLink],
+    imports: [MapComponent, RouterLink, PlayerInfoComponent, PlayersListComponent],
     templateUrl: './play-page.component.html',
     styleUrl: './play-page.component.scss',
 })
