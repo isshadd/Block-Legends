da36916283315a138a02c7d7ebdd400791e9f50c
Pablo <pablo.cabale@polymtl.ca>
2024-10-30 21:36:38 -0400

    Feat: server side spawn selection

--- server/app/gateways/playGameBoard/play-game-board.gateway.ts
+++ server/app/gateways/playGameBoard/play-game-board.gateway.ts
@@ -1,8 +1,7 @@
-import { GameSocketRoomService } from '@app/services/gateway-services/game-socket-room/game-socket-room.service';
 import { PlayGameBoardSocketService } from '@app/services/gateway-services/play-game-board-socket/play-game-board-socket.service';
 import { Injectable, Logger } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 @Injectable()
@@ -10,15 +9,10 @@ export class PlayGameBoardGateway {
     @WebSocketServer() server: Server;
     private readonly logger = new Logger(PlayGameBoardGateway.name);
 
-    constructor(
-        private readonly gameSocketRoomService: GameSocketRoomService,
-        private readonly playGameBoardSocketService: PlayGameBoardSocketService,
-    ) {}
+    constructor(private readonly playGameBoardSocketService: PlayGameBoardSocketService) {}
 
-    @SubscribeMessage('testRoomMessage')
-    testRoomMessage(client: Socket, accessCode: number) {
-        this.server.to(accessCode.toString()).emit('roomMessage', {
-            message: 'Hello from the room!',
-        });
+    startRoomGame(accessCode: number) {
+        this.playGameBoardSocketService.initRoomGameBoard(accessCode);
+        this.server.to(accessCode.toString()).emit('gameStarted');
     }
 }
--- server/app/services/gateway-services/play-game-board-socket/play-game-board-socket.service.ts
+++ server/app/services/gateway-services/play-game-board-socket/play-game-board-socket.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Game } from '@app/model/database/game';
+import { GameService } from '@app/services/game/game.service';
+import { MapSize } from '@common/enums/map-size';
+import { Injectable, Logger } from '@nestjs/common';
+import { GameRoom, GameSocketRoomService } from '../game-socket-room/game-socket-room.service';
+
+export interface GameBoardParameters {
+    game: Game;
+    spawnPlaces: Map<number, string>;
+}
 
 @Injectable()
-export class PlayGameBoardSocketService {}
+export class PlayGameBoardSocketService {
+    private readonly logger = new Logger(PlayGameBoardSocketService.name);
+    private gameBoardRooms: Map<number, GameBoardParameters> = new Map();
+
+    constructor(
+        private readonly gameService: GameService,
+        private readonly gameSocketRoomService: GameSocketRoomService,
+    ) {}
+
+    initRoomGameBoard(accessCode: number) {
+        const room = this.gameSocketRoomService.getRoomByAccessCode(accessCode);
+
+        if (!room) {
+            this.logger.error(`Room not found for access code: ${accessCode}`);
+            return;
+        }
+
+        let game: Game;
+        this.gameService.getGame(room.id).then((game) => {
+            game = game;
+        });
+
+        this.setupSpawnPoints(room, game);
+    }
+
+    setupSpawnPoints(room: GameRoom, game: Game) {
+        let spawnCounter = this.setSpawnCounter(game.size);
+        let spawnPlaces: Map<number, string> = new Map();
+        let availableSpawnPoints = spawnCounter;
+
+        for (let player of room.players) {
+            let assigned = false;
+
+            while (!assigned && availableSpawnPoints > 0) {
+                const randomIndex = Math.floor(Math.random() * spawnCounter);
+
+                if (!spawnPlaces.has(randomIndex)) {
+                    spawnPlaces.set(randomIndex, player.socketId);
+                    assigned = true;
+                    availableSpawnPoints--;
+                }
+            }
+        }
+
+        this.gameBoardRooms.set(room.accessCode, { game, spawnPlaces });
+    }
+
+    setSpawnCounter(gameSize: MapSize): number {
+        switch (gameSize) {
+            case MapSize.SMALL:
+                return 2;
+            case MapSize.MEDIUM:
+                return 4;
+            case MapSize.LARGE:
+                return 6;
+        }
+    }
+}
