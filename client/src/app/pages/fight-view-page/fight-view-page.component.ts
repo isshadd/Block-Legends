52a8d89897ef55b61b499871d9bd613caaf5e8cc
iremyildizz <iremyildiz02@gmail.com>
2024-10-30 21:50:58 -0400

    feat:Attack component is done

--- client/src/app/pages/fight-view-page/fight-view-page.component.ts
+++ client/src/app/pages/fight-view-page/fight-view-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FightViewComponent } from "../../components/play-area/fight-view/fight-view.component";
+import { PlayerCharacter } from '@app/classes/Characters/player-character';
+import { AvatarEnum } from '@common/enums/avatar-enum';
+import { FightViewComponent } from '../../components/play-area/fight-view/fight-view.component';
 
 @Component({
     selector: 'app-fight-view-page',
@@ -8,4 +10,17 @@ import { FightViewComponent } from "../../components/play-area/fight-view/fight-
     templateUrl: './fight-view-page.component.html',
     styleUrl: './fight-view-page.component.scss',
 })
-export class FightViewPageComponent {}
+export class FightViewPageComponent {
+    playerCharacter = new PlayerCharacter('Player1');
+    playerCharacter2 = new PlayerCharacter('Player2');
+
+    constructor() {
+        this.playerCharacter.avatar = AvatarEnum.Sirene;
+        this.playerCharacter.assignAttackDice();
+        this.playerCharacter.isLifeBonusAssigned = true;
+
+        this.playerCharacter2.avatar = AvatarEnum.Steve;
+        this.playerCharacter2.assignAttackDice();
+        this.playerCharacter2.isLifeBonusAssigned = true;
+    }
+}
