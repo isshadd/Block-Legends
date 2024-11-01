c8df68fc3f96269a101dc1a806933a7bcccc1335
Pablo <pablo.cabale@polymtl.ca>
2024-10-31 20:53:18 -0400

    Test: Terrain-tile

--- client/src/app/classes/Tiles/terrain-tile.spec.ts
+++ client/src/app/classes/Tiles/terrain-tile.spec.ts
@@ -1,3 +1,4 @@
+import { PlayerMapEntity } from '../Characters/player-map-entity';
 import { TerrainTile } from './terrain-tile';
 
 describe('TerrainTile', () => {
@@ -13,5 +14,13 @@ describe('TerrainTile', () => {
 
     it('should return true for isTerrain()', () => {
         expect(terrainTile.isTerrain()).toBeTrue();
+    });
+
+    it('should set player', () => {
+        const player = new PlayerMapEntity('test');
+        spyOn(player, 'setCoordinates');
+        terrainTile.setPlayer(player);
+        expect(terrainTile.player).toBe(player);
+        expect(player.setCoordinates).toHaveBeenCalledWith(terrainTile.coordinates);
     });
 });
