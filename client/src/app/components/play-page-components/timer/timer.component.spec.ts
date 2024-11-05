9b17e613b0c0fbe5bfcaad0fb9270f249914fee9
iremyildizz <iremyildiz02@gmail.com>
2024-11-05 15:16:05 -0500

    test: Timer

--- client/src/app/components/play-page-components/timer/timer.component.spec.ts
+++ client/src/app/components/play-page-components/timer/timer.component.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TimerComponent } from './timer.component';
 
 describe('TimerComponent', () => {
-  let component: TimerComponent;
-  let fixture: ComponentFixture<TimerComponent>;
+    let component: TimerComponent;
+    let fixture: ComponentFixture<TimerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TimerComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TimerComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(TimerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TimerComponent);
+        component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should return "Combat en cours" when isBattle is true', () => {
+        component.isBattle = true;
+        const formattedTime = component.formatTime();
+
+        expect(formattedTime).toBe('Combat en cours');
+    });
+
+    it('should return formatted time with player name and seconds when isBattle is false', () => {
+        component.isBattle = false;
+        component.playerTurnName = 'Player 1';
+        component.seconds = 30;
+
+        const formattedTime = component.formatTime();
+
+        expect(formattedTime).toBe('Player 1 30');
+    });
 });
