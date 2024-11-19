f98d4cc49647fcea0018eff21f420ae7fcfcceb0
Pablo <pablo.cabale@polymtl.ca>
2024-11-19 15:42:44 -0500

    Fix: item container works

--- client/src/app/components/play-page-components/item-list-container/item-list-container/item-list-container.component.ts
+++ client/src/app/components/play-page-components/item-list-container/item-list-container/item-list-container.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { PlaceableEntityComponent } from '@app/components/game-board-components/placeable-entity/placeable-entity.component';
+import { VisibleStateComponent } from '@app/components/game-board-components/visible-state/visible-state.component';
+import { Item } from '@common/classes/Items/item';
+import { VisibleState } from '@common/interfaces/placeable-entity';
 
 @Component({
-  selector: 'app-item-list-container',
-  standalone: true,
-  imports: [],
-  templateUrl: './item-list-container.component.html',
-  styleUrl: './item-list-container.component.scss'
+    selector: 'app-item-list-container',
+    standalone: true,
+    imports: [PlaceableEntityComponent, MatTooltipModule, VisibleStateComponent],
+    templateUrl: './item-list-container.component.html',
+    styleUrl: './item-list-container.component.scss',
 })
 export class ItemListContainerComponent {
+    @Input() containerItems: Item[];
+    @Output() itemClicked: EventEmitter<Item> = new EventEmitter<Item>();
 
+    onMouseEnter(item: Item) {
+        item.visibleState = VisibleState.Hovered;
+    }
+
+    onMouseLeave(item: Item) {
+        item.visibleState = VisibleState.NotSelected;
+    }
+
+    onMouseDown(event: MouseEvent, item: Item) {
+        if (event.button === 2) return;
+
+        this.itemClicked.emit(item);
+        event.preventDefault();
+    }
 }
