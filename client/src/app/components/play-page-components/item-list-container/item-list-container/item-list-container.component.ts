0833bc9c4ebadd7b0317cf2bf92972a013aa13bd
Pablo <pablo.cabale@polymtl.ca>
2024-11-19 15:23:19 -0500

    Feat: generation of item list container

---
+++ client/src/app/components/play-page-components/item-list-container/item-list-container/item-list-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-list-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-list-container.component.html',
+  styleUrl: './item-list-container.component.scss'
+})
+export class ItemListContainerComponent {
+
+}
