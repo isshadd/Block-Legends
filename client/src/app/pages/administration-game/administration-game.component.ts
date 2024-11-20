7329b365bc23ff228629c5ba21152161da2fe28f
Nihal-T <taihnihal1@gmail.com>
2024-11-20 17:02:43 -0500

    feat: le boutton import permet de sélectionner un fichier de formt json

--- client/src/app/pages/administration-game/administration-game.component.ts
+++ client/src/app/pages/administration-game/administration-game.component.ts
@@ -10,12 +10,28 @@ import { ListGameComponent } from '@app/components/administration-page-component
     standalone: true,
     imports: [CommonModule, RouterLink, ListGameComponent],
     templateUrl: './administration-game.component.html',
-    styleUrl: './administration-game.component.scss',
+    styleUrls: ['./administration-game.component.scss'],
 })
 export class AdministrationGameComponent {
+    fileList: File[] = [];
     constructor(public dialog: MatDialog) {}
 
     openCreateGameModal(): void {
         this.dialog.open(CreateGameModalComponent);
+    }
+
+    triggerFileInput(): void {
+        const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+        if (fileInput) {
+            fileInput.click();
+        }
+    }
+
+    onFileChange(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        if (input.files && input.files.length > 0) {
+            this.fileList = Array.from(input.files);
+            console.log('Fichiers sélectionnés :', this.fileList);
+        }
     }
 }
