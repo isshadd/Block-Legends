4de3ae065c383d8e720ee6a94764fd902952044e
iremyildizz <iremyildiz02@gmail.com>
2024-12-01 17:28:51 -0500

    fix: pourcentage terrain tile visited

--- client/src/app/services/play-page-services/game-statistics.service.ts
+++ client/src/app/services/play-page-services/game-statistics.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { PlayerCharacter } from '@common/classes/Player/player-character';
 import { DoorTile } from '@common/classes/Tiles/door-tile';
-import { GrassTile } from '@common/classes/Tiles/grass-tile';
 import { OpenDoor } from '@common/classes/Tiles/open-door';
 import { Tile } from '@common/classes/Tiles/tile';
-import { AvatarEnum } from '@common/enums/avatar-enum';
-import { ItemType } from '@common/enums/item-type';
 import { GameStatistics } from '@common/interfaces/game-statistics';
 import { GameMapDataManagerService } from '../game-board-services/game-map-data-manager.service';
 
@@ -30,57 +27,7 @@ export class GameStatisticsService {
     };
     currentGrid: Tile[][] = [];
 
-    constructor(public gameMapDataManagerService: GameMapDataManagerService) {
-        const player1 = new PlayerCharacter('Player 1');
-        const player2 = new PlayerCharacter('Player 2');
-
-        player1.avatar = AvatarEnum.Alex;
-        player1.totalCombats = 2;
-        player1.totalEvasions = 2;
-        player1.fightWins = 1;
-        player1.fightLoses = 1;
-        player1.totalLostLife = 10;
-        player1.totalDamageDealt = 11;
-        player1.differentItemsGrabbed = [ItemType.Totem, ItemType.Flag];
-        player1.differentTerrainTilesVisited = [
-            { x: 1, y: 1 },
-            { x: 2, y: 2 },
-        ];
-
-        player2.avatar = AvatarEnum.Arlina;
-        player2.totalCombats = 3;
-        player2.totalEvasions = 3;
-        player2.fightWins = 2;
-        player2.fightLoses = 0;
-        player2.totalLostLife = 5;
-        player2.totalDamageDealt = 20;
-        player2.differentItemsGrabbed = [ItemType.Totem];
-        player2.differentTerrainTilesVisited = [
-            { x: 1, y: 1 },
-            { x: 2, y: 2 },
-            { x: 3, y: 3 },
-        ];
-
-        this.gameStatistics.players.push(player1);
-        this.gameStatistics.players.push(player2);
-        this.gameStatistics.isGameOn = true;
-        this.gameStatistics.totalGameTime = 3600;
-        this.gameStatistics.totalPlayerTurns = 2;
-        this.gameStatistics.totalTerrainTilesVisited = [
-            { x: 1, y: 1 },
-            { x: 2, y: 2 },
-            { x: 3, y: 3 },
-            { x: 2, y: 2 },
-        ];
-        this.gameStatistics.totalDoorsInteracted = [{ x: 2, y: 2 }];
-        this.gameStatistics.totalPlayersThatGrabbedFlag = ['Player 1'];
-
-        this.currentGrid = [
-            [new GrassTile(), new GrassTile(), new GrassTile(), new DoorTile()],
-            [new GrassTile(), new DoorTile(), new GrassTile(), new GrassTile()],
-            [new GrassTile(), new GrassTile(), new DoorTile(), new GrassTile()],
-        ];
-    }
+    constructor(public gameMapDataManagerService: GameMapDataManagerService) {}
 
     initGameStatistics(newGameStatistics: GameStatistics) {
         console.log(newGameStatistics);
@@ -101,6 +48,7 @@ export class GameStatisticsService {
             case SortCharacters.FightLoses:
                 this.gameStatistics.players.sort((a, b) => b.fightLoses - a.fightLoses);
                 break;
+
             default:
                 break;
         }
@@ -125,11 +73,15 @@ export class GameStatisticsService {
         }
     }
 
-    getTotalTilePercentage() {
+    getTilePercentage(player?: PlayerCharacter): number {
         let gridLength: number = 0;
         for (let column of this.gameMapDataManagerService.getCurrentGrid()) {
             gridLength += column.length;
         }
+        if (player) {
+            return gridLength === 0 ? 0 : Math.round((player.differentTerrainTilesVisited.length / gridLength) * 100);
+        }
+
         return gridLength === 0 ? 0 : Math.round((this.gameStatistics.totalTerrainTilesVisited.length / gridLength) * 100);
     }
 
