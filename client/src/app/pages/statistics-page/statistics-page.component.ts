13289d162b2926c1fa7fbf3e507d13cfe503f58a
Pablo <pablo.cabale@polymtl.ca>
2024-12-01 17:39:47 -0500

    Fix: leaveGame and remove of main menu button

--- client/src/app/pages/statistics-page/statistics-page.component.ts
+++ client/src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { ArrowDownButtonComponent } from '@app/components/arrow-down-button/arrow-down-button.component';
 import { ArrowUpButtonComponent } from '@app/components/arrow-up-button/arrow-up-button.component';
 import { ClavardageComponent } from '@app/components/clavardage/clavardage.component';
 import { GameMapDataManagerService } from '@app/services/game-board-services/game-map-data-manager.service';
+import { PlayGameBoardSocketService } from '@app/services/play-page-services/game-board/play-game-board-socket.service';
 import { GameStatisticsService, SortCharacters } from '@app/services/play-page-services/game-statistics.service';
 
 @Component({
     selector: 'app-statistics-page',
     standalone: true,
-    imports: [ClavardageComponent, RouterLink, ArrowUpButtonComponent, ArrowDownButtonComponent],
+    imports: [ClavardageComponent, ArrowUpButtonComponent, ArrowDownButtonComponent],
     templateUrl: './statistics-page.component.html',
     styleUrl: './statistics-page.component.scss',
 })
@@ -18,6 +18,7 @@ export class StatisticsPageComponent {
     constructor(
         public gameStatisticsService: GameStatisticsService,
         public gameMapDataManagerService: GameMapDataManagerService,
+        public playGameBoardSocketService: PlayGameBoardSocketService,
     ) {}
 
     formatGameTime(totalGameTime: number): string {
@@ -30,5 +31,9 @@ export class StatisticsPageComponent {
 
     padWithZero(value: number): string {
         return value.toString().padStart(2, '0');
+    }
+
+    returnHome(): void {
+        this.playGameBoardSocketService.leaveGame();
     }
 }
